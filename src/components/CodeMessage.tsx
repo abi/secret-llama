280249689dbf09c73f4965cdc1f356d7abf03196
arthur <arthurjdam@gmail.com>
2024-05-04 17:56:08 -0700

    feat: add language header and "Copy code" button

--- src/components/CodeMessage.tsx
+++ src/components/CodeMessage.tsx
@@ -1,4 +1,7 @@
-import { type ReactNode } from 'react';
+import { useTimedToggle } from '@/hooks/useTimedToggle';
+import { copyTextToClipboard } from '@/lib/utils';
+import { FaCheck, FaRegClipboard } from 'react-icons/fa6';
+import { useCallback, type ReactNode } from 'react';
 import {
     Prism as SyntaxHighlighter,
     type SyntaxHighlighterProps,
@@ -14,14 +17,42 @@ function CodeMessage({
     className?: string;
 } & Partial<Omit<SyntaxHighlighterProps, 'children'>>) {
     const match = /language-(\w+)/.exec(className || '');
+    const [copied, setCopied] = useTimedToggle(3000);
+
+    const onClickCopy = useCallback(() => {
+        setCopied();
+        copyTextToClipboard(String(children));
+    }, [children, setCopied]);
+
     return match ? (
-        <SyntaxHighlighter
-            {...rest}
-            PreTag="div"
-            children={String(children).replace(/\n$/, '')}
-            language={match[1]}
-            style={vscDarkPlus}
-        />
+        <>
+            <header className="text-xs flex">
+                <span className="text-xs text-slate-500">{match[1]}</span>
+                <button
+                    className="ml-auto text-slate-400 hover:text-slate-100 flex gap-2"
+                    onClick={onClickCopy}
+                >
+                    {copied ? (
+                        <>
+                            <FaCheck />
+                            <span>Copied!</span>
+                        </>
+                    ) : (
+                        <>
+                            <FaRegClipboard />
+                            <span>Copy code</span>
+                        </>
+                    )}
+                </button>
+            </header>
+            <SyntaxHighlighter
+                {...rest}
+                PreTag="div"
+                children={String(children).replace(/\n$/, '')}
+                language={match[1]}
+                style={vscDarkPlus}
+            />
+        </>
     ) : (
         <code {...rest} className={className}>
             {children}
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export async function copyTextToClipboard(text: string): Promise<void> {
+  if ('clipboard' in navigator) {
+    await navigator.clipboard.writeText(text);
+  } else {
+    document.execCommand('copy', true, text);
+  }
+}
