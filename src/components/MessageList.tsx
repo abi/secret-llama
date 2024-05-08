62763edbbc26168b0c5091c57ff27ce79e611bd5
jonbo <john@johnbartel.us>
2024-05-07 23:02:26 -0500

    Improved autoscrolling behavior

--- src/components/MessageList.tsx
+++ src/components/MessageList.tsx
@@ -1,21 +1,32 @@
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import useChatStore from "../hooks/useChatStore";
 import Message from "./Message";
 
 function MessageList() {
   const chatHistory = useChatStore((state) => state.chatHistory);
-
   const scrollRef = useRef<HTMLDivElement | null>(null);
+  const [enableAutoScroll, setEnableAutoScroll] = useState<boolean>(true);
 
-  // Scroll to the bottom of the chat history when it changes
+  // Scroll to the bottom of the chat history when it changes, if at the bottom
   useEffect(() => {
-    if (scrollRef.current) {
+    if (scrollRef.current && enableAutoScroll) {
       scrollRef.current.scrollTop = scrollRef.current.scrollHeight;
     }
-  }, [chatHistory]);
+  }, [chatHistory, enableAutoScroll]);
+
+  function onChatScroll(event: React.UIEvent<HTMLDivElement>) {
+    const { scrollTop, clientHeight, scrollHeight } =
+      event.target as HTMLDivElement;
+    const isAtBottom = scrollHeight - scrollTop === clientHeight;
+    setEnableAutoScroll(isAtBottom);
+  }
 
   return (
-    <div className="flex-1 overflow-auto" ref={scrollRef}>
+    <div
+      className="flex-1 overflow-auto"
+      ref={scrollRef}
+      onScroll={onChatScroll}
+    >
       <div className="max-w-3xl mx-auto text-base px-5">
         {chatHistory.map((message, index) => (
           <Message key={index} message={message} />
